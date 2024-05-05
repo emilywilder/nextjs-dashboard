29127a37fff583b45460a995b27e9232c211708d
Emily Lockhart <validitymaiden@gmail.com>
2024-05-05 15:53:35 -0700

    remove erroneous import

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,8 +5,6 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { string } from 'zod';
 
 export default async function Page({
   searchParams,
