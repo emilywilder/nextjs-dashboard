07e1ca74f6564e6a2c08fb5d84aba16fb1fd1b1f
Emily Lockhart <validitymaiden@gmail.com>
2024-05-06 13:02:42 -0700

    fix typo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,7 +12,7 @@ const FormSchema = z.object({
   }),
   amount: z.coerce
     .number()
-    .gt(0, { message: 'Please enter an amount greater that $0.' }),
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
   }),
