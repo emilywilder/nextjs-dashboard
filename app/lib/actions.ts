601b7a66811190bd30f9bb7a34418b389d5485bb
Emily Lockhart <validitymaiden@gmail.com>
2024-05-05 19:38:20 -0700

    use zod for validation specify a schema, then use that schema to create another schmea for taking information from the raw form data manipulate the amount to be in cents create a date

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,30 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+  // reduce likelihood of float point errors
+  const amountInCents = amount * 100;
+  // create date with format 'YYYY-MM-DD'
+  const date = new Date().toISOString().split('T')[0];
   // another method:
   // const rawFormData = Object.fromEntries(formData.entries())
-  console.log(rawFormData);
+  console.log(
+    `customerId: ${customerId}, amount: ${amountInCents}, status: ${status}, date: ${date}`,
+  );
 }
