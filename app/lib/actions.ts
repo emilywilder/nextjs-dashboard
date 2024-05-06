c2170620e4f0910279fbfd125e7bb7856198a21d
Emily Lockhart <validitymaiden@gmail.com>
2024-05-05 19:28:06 -0700

    get formData from <Form> in createInvoice()

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,12 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // another method:
+  // const rawFormData = Object.fromEntries(formData.entries())
+  console.log(rawFormData);
+}
